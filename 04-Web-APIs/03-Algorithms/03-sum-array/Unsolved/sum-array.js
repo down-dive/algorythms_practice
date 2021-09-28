9f0e232a6ec9df3a7b984314c3edc0ee8b72df62
Jane Terlyuk <jter1990@gmail.com>
2021-09-28 10:05:52 -0700

    trying out on fizz buzz

--- 04-Web-APIs/03-Algorithms/03-sum-array/Unsolved/sum-array.js
+++ 04-Web-APIs/03-Algorithms/03-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,18 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+// let arr = [1,3,5]
+// let number = 0
+// var sumArray = function(arr) {
+//     for(let i = 0; i < arr.length; i++){
+//         number += arr[i];
+//     }
+// };
+// console.log(number)
+
+var sumArray = function(arr) {
+    arr.reduce(function(a, b) {
+
+   console.log(a + b)
+}, 0)
+}
+
