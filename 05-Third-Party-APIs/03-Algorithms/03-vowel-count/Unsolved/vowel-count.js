7ed345ae40995dd091f68e1f896fb7bef4f9bd26
Jane Terlyuk <jter1990@gmail.com>
2021-09-28 15:30:45 -0700

    trying out vowel count, max-num is completed but there is a weird error

--- 05-Third-Party-APIs/03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
+++ 05-Third-Party-APIs/03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,8 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+
+    array = str.split();
+    console.log(array)
+
+};
