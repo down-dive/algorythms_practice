81968429e07b85d0d382339cbd38d34299ac8ee5
Jane Terlyuk <jter1990@gmail.com>
2021-09-30 11:08:45 -0700

    trying on vowel-count

--- 05-Third-Party-APIs/03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
+++ 05-Third-Party-APIs/03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
@@ -1,8 +1,15 @@
 // Write code to return the the number of vowels in `str`
 
 var vowelCount = function(str) {
+    var count = 0;
+    var vowels = ["a", "e", "i", "o", "u"];
 
-    array = str.split();
-    console.log(array)
+    for (var i = 0; i < str.lenght; i++) {
+        var letter = str[i].toLowerCase();
 
+        if (vowels.indexOf(letter) !== -1) {
+            count += 1;
+        }
+    }
+    return count;
 };
