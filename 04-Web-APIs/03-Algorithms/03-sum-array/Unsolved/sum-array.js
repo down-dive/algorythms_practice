80cd3e574699b5b9ca82e7c29d2b8bd6687feef1
Jane Terlyuk <jter1990@gmail.com>
2021-10-03 16:09:24 -0700

    second version of sum array using reduce method defining functions with takes two parameters arr.reduce(function(a,b) and returns sum of two parameters: a + b, the important part is to return the variable to was set equial to that function

--- 04-Web-APIs/03-Algorithms/03-sum-array/Unsolved/sum-array.js
+++ 04-Web-APIs/03-Algorithms/03-sum-array/Unsolved/sum-array.js
@@ -1,20 +1,21 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {
-    let number = 0
-    for(let i = 0; i < arr.length; i++){
-        if(isNaN(arr[i])){
-            continue
-        } 
-        number += arr[i];
-    }
-    return number
-};
-
-
 // var sumArray = function(arr) {
-//     arr.reduce(function(a, b) {
-//    console.log(a + b)
-// }, 0)
-// }
+//     let number = 0
+//     for(let i = 0; i < arr.length; i++){
+//         if(isNaN(arr[i])){
+//             continue
+//         } 
+//         number += arr[i];
+//     }
+//     return number
+// };
+
+
+var sumArray = function(arr) {
+    let sum = arr.reduce(function(a, b) {
+   return a + b;
+}, 0)
+return sum
+}
 
