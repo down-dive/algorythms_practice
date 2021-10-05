aac30677d731cc3d021ca69e5aeda4f44ac673df
Jane Terlyuk <jter1990@gmail.com>
2021-10-05 09:08:05 -0700

    palindrome is covered, need to remember that reverse method is for arrays only and before using it need to change strin into array with split(') method and join(') to change it back to string

--- 06-Server-Side-APIs/03-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
+++ 06-Server-Side-APIs/03-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
@@ -1,11 +1,25 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {
-    var reverse = str.split("").reverse().join("")
+// var isPalindrome = function(str) {
+//     var reverse = str.split("").reverse().join("")
 
-    if ( reverse === str) {
-        return true;
-    } else {
-        return false;
-    }
-};
+//     if ( reverse === str) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// };
+
+var isPalindrome = function(str) {
+    var reversed = str.split('').reverse().join('')
+    
+    if (str === reversed) {
+      console.log("It worked!")
+      return true
+      
+  } else {
+    console.log("It is not palindrome")
+    return false
+    
+  }
+  }
