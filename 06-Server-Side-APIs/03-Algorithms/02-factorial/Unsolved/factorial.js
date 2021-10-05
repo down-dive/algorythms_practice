094fc1788d440e7801ac9a993d84b7c66d486e00
Jane Terlyuk <jter1990@gmail.com>
2021-10-05 10:30:30 -0700

    factorial is all about understanding how for loop works. There is two solutions: 1. To start iterating from number 2 and make i less than or equial to num or 2. to start iterating from the num, i is more than 1 and i--, this way we iterating from the num dont to 1

--- 06-Server-Side-APIs/03-Algorithms/02-factorial/Unsolved/factorial.js
+++ 06-Server-Side-APIs/03-Algorithms/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,10 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function(num) {
+    var startValue = 1;
+    
+    for (var i = 1; i <= num; i++) {
+      startValue = startValue * i;
+    }
+    return startValue
+  }
