8b35c0bf57d8a227d4cc1b69e8cbd5a6520888a8
Jane Terlyuk <jter1990@gmail.com>
2021-10-05 10:32:20 -0700

    adding second solution

--- 06-Server-Side-APIs/03-Algorithms/02-factorial/Unsolved/factorial.js
+++ 06-Server-Side-APIs/03-Algorithms/02-factorial/Unsolved/factorial.js
@@ -1,10 +1,19 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {
-    var startValue = 1;
+// var factorial = function(num) {
+//     var startValue = 1;
     
-    for (var i = 1; i <= num; i++) {
-      startValue = startValue * i;
+//     for (var i = 1; i <= num; i++) {
+//       startValue = startValue * i;
+//     }
+//     return startValue
+//   }
+
+var factorial = function(num) {
+    var result = 1;
+
+    for ( var i = num; i > 1; i--) {
+        result = result * i;
     }
-    return startValue
-  }
+    return result;
+}
