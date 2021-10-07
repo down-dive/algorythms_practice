85264256c610c2f40cc6e215b18b3dac98c11f12
Yevgeniya Terlyuk <jter1990@gmail.com>
2021-10-06 21:05:44 -0700

    trying out dom

--- 04-Web-APIs/01-Activities/01-DOM-manipulation/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/01-DOM-manipulation/Unsolved/assets/js/script.js
@@ -4,7 +4,22 @@ var body = document.body;
 
 // TODO: Add a centered h1
 
+var hello = document.createElement("h1");
+hello.setAttribute("id","hello");
+document.getElementById('hello').align = 'center';
+hello.textContent = "hello there!";
+body.appendChild(hello);
+
+
+
 // TODO: Add a centered h2
+
+var secondLine = document.createElement("h2");
+secondLine.setAttribute("id", "secondLine");
+document.getElementById("secondLine").align = 'center';
+secondLine.textContent = "this is my second line";
+hello.appendChild(secondLine);
+
 
 // TODO: Add a centered image with a centered caption under it
 
