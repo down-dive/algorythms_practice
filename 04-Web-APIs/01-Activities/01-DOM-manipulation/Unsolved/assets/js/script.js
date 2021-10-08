ed1d61d9e31d2d4a92369650dc97a9c71cc33995
Yevgeniya Terlyuk <jter1990@gmail.com>
2021-10-07 20:48:47 -0700

    creating bio from scratch. practicing html sintax

--- 04-Web-APIs/01-Activities/01-DOM-manipulation/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/01-DOM-manipulation/Unsolved/assets/js/script.js
@@ -5,22 +5,28 @@ var body = document.body;
 // TODO: Add a centered h1
 
 var hello = document.createElement("h1");
-hello.setAttribute("id","hello");
-document.getElementById('hello').align = 'center';
 hello.textContent = "hello there!";
 body.appendChild(hello);
+hello.setAttribute("id","hello");
+document.getElementById('hello').align = 'center';
 
 
 
 // TODO: Add a centered h2
 
 var secondLine = document.createElement("h2");
-secondLine.setAttribute("id", "secondLine");
-document.getElementById("secondLine").align = 'center';
 secondLine.textContent = "this is my second line";
 hello.appendChild(secondLine);
+secondLine.setAttribute("id", "secondLine");
+document.getElementById("secondLine").align = 'center';
+
 
 
 // TODO: Add a centered image with a centered caption under it
+var image = document.createElement("img");
+image.src = "https://media.geeksforgeeks.org/wp-content/uploads/20190529122828/bs21.png";
+secondLine.appendChild(image);
+image.setAttribute("id", "image");
+document.getElementById("image").align = 'center';
 
 // TODO: Add a list of your favorite foods (or other favorites)
